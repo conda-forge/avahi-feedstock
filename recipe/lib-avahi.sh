aa7e2fd0d0b76a2e54c8c5f43f9e03044c230072
Duncan Macleod <duncan.macleod@ligo.org>
2021-11-08 08:48:09 -0800

    lib-avahi.sh: general script maintenance
    use `make V=1 VERBOSE=1` to aid debugging failures
    run `make check` (not when cross compiling)

--- recipe/lib-avahi.sh
+++ recipe/lib-avahi.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 set -xou
 
 # update config.sub and config.guess
@@ -7,21 +9,31 @@ mkdir build
 pushd build
 
 EXTRA_DISABLES=""
-NPROC=$CPU_COUNT
 if [[ $(uname) == "Darwin" ]]; then
     export CFLAGS="${CFLAGS} -D__APPLE_USE_RFC_2292"
-    EXTRA_DISABLES="--disable-autoipd"
+    EXTRA_DISABLES="${EXTRA_DISABLES} --disable-autoipd"
 fi
 
+# configure
+${SRC_DIR}/configure \
+    --prefix $PREFIX \
+    --libdir ${PREFIX}/lib \
+    --bindir ${PREFIX}/bin \
+    --disable-qt3 \
+    --disable-qt5 \
+    --disable-gtk3 \
+    --disable-gdbm \
+    --disable-python \
+    --disable-mono \
+    ${EXTRA_DISABLES} \
+;
 
-../configure --prefix $PREFIX \
-             --libdir ${PREFIX}/lib \
-             --bindir ${PREFIX}/bin \
-             --disable-qt5 \
-             --disable-gtk3 \
-             --disable-gdbm \
-             --disable-python \
-             --disable-mono $EXTRA_DISABLES
+# build
+make -j ${CPU_COUNT} V=1 VERBOSE=1
 
-make -j${NPROC}
-make install
+# test (not when cross compiling)
+if [[ "${build_platform}" == "${target_platform}" ]]; then
+    make -j ${CPU_COUNT} V=1 VERBOSE=1 check
+fi
+
+make -j ${CPU_COUNT} V=1 VERBOSE=1 install
